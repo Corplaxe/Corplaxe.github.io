e00b8db391a1026f3b53f1b71591cfe6fdfb802e
Corplaxe83 <61080511+Mumya@users.noreply.github.com>
2021-05-22 21:19:29 +0300

    Update sohbet.php

--- chat/sohbet.php
+++ chat/sohbet.php
@@ -48,16 +48,10 @@
                 </div>
             </div>
         </div>
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/8.6.2/firebase-analytics.js"></script>
-
+<script src="https://www.gstatic.com/firebasejs/5.8.3/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.8.2/firebase-database.js"></script>
 <script>
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
   var firebaseConfig = {
     apiKey: "AIzaSyAU-FU3Xch8VSVoQTp225-w8rlLYon2Kq8",
     authDomain: "vital-cedar-312014.firebaseapp.com",
